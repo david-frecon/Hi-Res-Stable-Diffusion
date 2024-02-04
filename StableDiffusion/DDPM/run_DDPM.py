a7f1845239a12e4ee7cf4d80364182c11d358016
Louis <louis.pagnier@epita.fr>
2024-02-04 17:31:26 +0100

    Save video option for DDPM

--- StableDiffusion/DDPM/run_DDPM.py
+++ StableDiffusion/DDPM/run_DDPM.py
@@ -1,17 +1,17 @@
 import torch
 
 from StableDiffusion.UNet.UNet import UNet
-from StableDiffusion.utils import test_DDPM_chain, to_device
+from StableDiffusion.utils import test_DDPM_chain, to_device, get_device_name
 
 MODEL_DATE = "2024-02-01_22:50:52"
 T_MAX = 400
 BETA = 0.0001
-MODEL_PATH = f"models/unet_{T_MAX}_{MODEL_DATE}.pth"
-MODEL_PATH = "models/tmp_best.pth"
+MODEL_PATH = f"../../models/unet_{T_MAX}_{MODEL_DATE}.pth"
+MODEL_PATH = "../../models/tmp_best_louis.pth"
 
 model = UNet(depth=4, time_emb_dim=32, color_channels=3)
-model.load_state_dict(torch.load(MODEL_PATH))
+model.load_state_dict(torch.load(MODEL_PATH, map_location=get_device_name()))
 model = to_device(model)
 
 model.eval()
-test_DDPM_chain(model, BETA, T_MAX, shape=(1, 3, 32, 32), n_samples=4)
+test_DDPM_chain(model, BETA, T_MAX, shape=(1, 3, 32, 32), n_samples=4, save_video=True)
--- StableDiffusion/utils.py
+++ StableDiffusion/utils.py
@@ -7,7 +7,7 @@ from rich.progress import Progress
 import torch
 from matplotlib import pyplot as plt
 
-from NoiseScheduler.NoiseScheduler import denormalize_img, alphas_schedule, betas_schedule, alphas_bar_schedule
+from StableDiffusion.NoiseScheduler.NoiseScheduler import denormalize_img, alphas_schedule, betas_schedule, alphas_bar_schedule
 
 
 @cache
@@ -29,7 +29,7 @@ def to_device(model):
 
 
 @torch.no_grad()
-def test_DDPM_chain(model, beta, max_t, shape=(1, 1, 28, 28), n_samples=4):
+def test_DDPM_chain(model, beta, max_t, shape=(1, 1, 28, 28), n_samples=4, save_video=False):
     big_chain = []
 
     betas = betas_schedule(beta, max_t)
@@ -57,14 +57,30 @@ def test_DDPM_chain(model, beta, max_t, shape=(1, 1, 28, 28), n_samples=4):
 
     fig, ax = plt.subplots(n_samples, 5)
     for i in range(n_samples):
-        ax[i, 0].imshow(denormalize_img(big_chain[i][-16].permute(0, 2, 3, 1).detach().cpu().squeeze()))
-        ax[i, 1].imshow(denormalize_img(big_chain[i][-8].permute(0, 2, 3, 1).detach().cpu().squeeze()))
-        ax[i, 2].imshow(denormalize_img(big_chain[i][-4].permute(0, 2, 3, 1).detach().cpu().squeeze()))
-        ax[i, 3].imshow(denormalize_img(big_chain[i][-2].permute(0, 2, 3, 1).detach().cpu().squeeze()))
-
-        ax[i, 4].imshow(denormalize_img(big_chain[i][-1].permute(0, 2, 3, 1).detach().cpu().squeeze()))
+        for j, t in enumerate([-16, -8, -4, -2, -1]):
+            ax[i, j].imshow(denormalize_img(big_chain[i][t].permute(0, 2, 3, 1).detach().cpu().squeeze()))
+            ax[i, j].axis("off")
 
     plt.show()
+
+    if save_video:
+        import cv2
+        vw = shape[2]
+        fourcc = cv2.VideoWriter_fourcc(*'avc1')
+        out = cv2.VideoWriter('output.mp4', fourcc, 30.0, (512 * n_samples, 512))
+        img = np.zeros((512, 512 * n_samples, 3), dtype=np.uint8)
+        for t in range(max_t // 2, max_t):
+            images = [big_chain[i][t] for i in range(n_samples)]
+            decoded_images = [denormalize_img(img.permute(0, 2, 3, 1).detach().cpu().squeeze()).astype(np.uint8) for img in images]
+            decoded_images = [cv2.cvtColor(img, cv2.COLOR_RGB2BGR) for img in decoded_images]
+            decoded_images = [cv2.resize(img, (512, 512), interpolation=cv2.INTER_NEAREST) for img in decoded_images]
+
+            for j, decoded_img in enumerate(decoded_images):
+                img[:, 512 * j:512 * (j + 1), :] = decoded_img
+
+            out.write(img)
+        out.release()
+
     return big_chain
 
 
