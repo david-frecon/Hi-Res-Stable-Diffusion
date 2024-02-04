be946e5ede36f5a467c270c62a00421d73caae0e
Louis <louis.pagnier@epita.fr>
2024-02-04 19:43:58 +0100

    Add app

---
+++ StableDiffusion/app.py
@@ -0,0 +1,133 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
+import torch
+
+from utils import test_stable_diffusion_chain, test_DDPM_chain, to_device, get_device_name
+from UNet.UNetText import UNetText
+from UNet.UNet import UNet
+from VAE.VAE import VAE
+from fashion_clip.fashion_clip import FashionCLIP
+
+BATCH_SIZE = 64
+BETA = 0.0001
+T_MAX = 1000
+
+STABLE_DIFFUSION_MODELS = {
+    "unet": "unet_text_color.pth",
+    "vae": "vae_louis.pt"
+}
+DDPM_MODELS = {
+    "unet": "tmp_best_louis.pth"
+}
+VAE_MODELS = {
+    "vae": "vae_louis.pt"
+}
+
+def create_app():
+    window = tk.Tk()
+    window.title("EPITA Stable Diffusion")
+    window.geometry("1000x800")
+
+    def update_ui(*args):
+        if mode.get() == "DDPM":
+            description_label.config(text="Générez des chevaux", font=("Arial", 14))
+            description_entry.pack_forget()
+        else:
+            description_label.config(text="Description:", font=("Arial", 14))
+            description_entry.pack(after=description_label)
+
+    def ui_callback(progress, new_image: Figure = None):
+        progress_bar["value"] = progress
+        if new_image is not None:
+            canvas.draw()
+        window.update()
+
+    def generate():
+        number_of_images = int(number_selector.get())
+        description = description_entry.get()
+        model = mode.get()
+        progress_bar["value"] = 0
+
+        if model == "StableDiffusion":
+            f_clip = FashionCLIP("fashion-clip")
+            texts_embeddings = to_device(torch.tensor(f_clip.encode_text([description] * number_of_images, batch_size=32))).view(number_of_images, 512)
+
+            unet = UNetText(depth=4, time_emb_dim=32, text_emb_dim=512, color_channels=1)
+            unet.load_state_dict(torch.load(f"../models/{STABLE_DIFFUSION_MODELS['unet']}", map_location=get_device_name()))
+            unet = to_device(unet)
+            unet.eval()
+            unet.requires_grad_(False)
+
+            vae = VAE(16**2)
+            vae.load_state_dict(torch.load(f"../models/{STABLE_DIFFUSION_MODELS['vae']}", map_location=get_device_name()))
+            vae = to_device(vae)
+            vae.eval()
+            vae.requires_grad_(False)
+
+            test_stable_diffusion_chain(unet, vae, BETA, T_MAX, texts_embeddings, 16, save_video=False, callback=ui_callback, fig=fig)
+
+        elif model == "DDPM":
+            unet = UNet(depth=4, time_emb_dim=32, color_channels=3)
+            unet.load_state_dict(torch.load(f"../models/{DDPM_MODELS['unet']}", map_location=get_device_name()))
+            unet = to_device(unet)
+            unet.eval()
+            unet.requires_grad_(False)
+
+            test_DDPM_chain(unet, BETA, T_MAX, (number_of_images, 3, 32, 32), number_of_images, save_video=False, callback=ui_callback, fig=fig)
+
+    left_panel = tk.Frame(window)
+    left_panel.pack(side=tk.LEFT, padx=10, pady=10, fill=tk.Y)
+
+    mode_label = tk.Label(left_panel, text="Modèle", font=("Arial", 14))
+    mode_label.pack(anchor=tk.W)
+
+    mode = tk.StringVar(value="StableDiffusion")
+    mode.trace_add("write", update_ui)
+    modes = ["StableDiffusion", "DDPM"]
+    for m in modes:
+        tk.Radiobutton(left_panel, text=m, variable=mode, value=m).pack(anchor=tk.W)
+
+    spacer = tk.Label(left_panel, text="")
+    spacer.pack()
+
+    number_label = tk.Label(left_panel, text="Nombre d'images:", font=("Arial", 14))
+    number_label.pack(anchor=tk.W)
+    number_selector = ttk.Spinbox(left_panel, from_=4, to=16, width=5)
+    number_selector.set(4)
+    number_selector.pack(anchor=tk.W)
+
+    center_frame = tk.Frame(window)
+    center_frame.pack(side=tk.LEFT, expand=True, fill=tk.BOTH, padx=20)
+
+    fig = Figure(figsize=(5, 4), dpi=100)
+    plot = fig.add_subplot(111)
+    plot.axis("off")
+    plot.plot()
+
+    canvas = FigureCanvasTkAgg(fig, master=center_frame)
+    canvas_widget = canvas.get_tk_widget()
+    canvas_widget.pack(side=tk.TOP, fill=tk.BOTH, expand=True, pady=15)
+
+    description_label = tk.Label(center_frame, text="Description", font=("Arial", 14))
+    description_label.pack()
+    description_entry = tk.Entry(center_frame, width=50)
+    description_entry.pack()
+    select_file_button = tk.Button(center_frame, text="Sélectionner une image",
+                                   command=lambda: filedialog.askopenfilename())
+
+    generate_button = tk.Button(center_frame, text="Générer", command=generate)
+    generate_button.pack(pady=10)
+
+    progress_bar = ttk.Progressbar(center_frame, orient="horizontal", mode="determinate", length=300)
+    progress_bar.pack(pady=10)
+
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    create_app()
--- StableDiffusion/utils.py
+++ StableDiffusion/utils.py
@@ -28,10 +28,28 @@ def to_device(model):
     return model.to(get_device())
 
 
-@torch.no_grad()
-def test_DDPM_chain(model, beta, max_t, shape=(1, 1, 28, 28), n_samples=4, save_video=False):
-    big_chain = []
+def get_grid_size(n_samples):
+    if n_samples == 4:
+        return 2, 2
+    if n_samples == 5 or n_samples == 6:
+        return 2, 3
+    if n_samples == 7 or n_samples == 8:
+        return 2, 4
+    if n_samples == 9:
+        return 3, 3
+    if n_samples == 10:
+        return 2, 5
+    if n_samples == 11 or n_samples == 12:
+        return 3, 4
+    if n_samples == 13 or n_samples == 14:
+        return 3, 5
+    if n_samples == 15 or n_samples == 16:
+        return 4, 4
+    return 1, 1
 
+
+@torch.no_grad()
+def test_DDPM_chain(model, beta, max_t, shape=(1, 1, 28, 28), n_samples=4, save_video=False, callback=None, fig=None):
     betas = betas_schedule(beta, max_t)
     alphas = alphas_schedule(betas)
     alphas_bar = alphas_bar_schedule(alphas)
@@ -39,38 +57,48 @@ def test_DDPM_chain(model, beta, max_t, shape=(1, 1, 28, 28), n_samples=4, save_
     inverse_sqrt_alphas = 1 / torch.sqrt(alphas)
     inverse_sqrt_alphas_bar = (1 - alphas) / torch.sqrt(1 - alphas_bar)
 
-    for i in range(n_samples):
-        full_noise = torch.randn(shape)
-        full_noise = to_device(full_noise)
-        chain = [full_noise]
+    full_noise = torch.randn(shape)
+    full_noise = to_device(full_noise)
+    chain = [full_noise]
 
-        for t in range(max_t - 1, -1, -1):
-            predicted_noise = model(chain[-1], torch.tensor([t]).to(get_device()).float())
-            new_img = inverse_sqrt_alphas[t] * (chain[-1] - inverse_sqrt_alphas_bar[t] * predicted_noise)
-            if t > 0:
-                noise = torch.randn(shape)
-                noise = to_device(noise)
-                new_img = new_img + torch.sqrt(betas[t]) * noise
-            new_img = torch.clip(new_img, -1, 1)
-            chain.append(new_img)
-        big_chain.append(chain)
+    for t in range(max_t - 1, -1, -1):
+        predicted_noise = model(chain[-1], torch.tensor([t] * n_samples).to(get_device()).float())
+        new_img = inverse_sqrt_alphas[t] * (chain[-1] - inverse_sqrt_alphas_bar[t] * predicted_noise)
+        if t > 0:
+            noise = torch.randn(shape)
+            noise = to_device(noise)
+            new_img = new_img + torch.sqrt(betas[t]) * noise
+        new_img = torch.clip(new_img, -1, 1)
+        chain.append(new_img)
 
-    fig, ax = plt.subplots(n_samples, 5)
-    for i in range(n_samples):
-        for j, t in enumerate([-16, -8, -4, -2, -1]):
-            ax[i, j].imshow(denormalize_img(big_chain[i][t].permute(0, 2, 3, 1).detach().cpu().squeeze()))
-            ax[i, j].axis("off")
+        if t % 10 == 0 and callback is not None:
+            l, c = get_grid_size(n_samples)
+            fig.clf()
+            ax = fig.subplots(l, c)
+            images = [chain[-1][i] for i in range(n_samples)]
+            decoded_images = [denormalize_img(img.permute(1, 2, 0).detach().cpu().squeeze()) for img in images]
+            for j, img in enumerate(decoded_images):
+                ax_index = (j // c, j % c)
+                ax[ax_index].imshow(img)
+                ax[ax_index].axis("off")
+            callback(100 - t / max_t * 100, fig)
 
-    plt.show()
+    if callback is None:
+        fig, ax = plt.subplots(n_samples, 5)
+        for i in range(n_samples):
+            for j, t in enumerate([-16, -8, -4, -2, -1]):
+                ax[i, j].imshow(denormalize_img(chain[t][i].permute(0, 2, 3, 1).detach().cpu().squeeze()))
+                ax[i, j].axis("off")
+
+        plt.show()
 
     if save_video:
         import cv2
-        vw = shape[2]
         fourcc = cv2.VideoWriter_fourcc(*'avc1')
         out = cv2.VideoWriter('output.mp4', fourcc, 30.0, (512 * n_samples, 512))
         img = np.zeros((512, 512 * n_samples, 3), dtype=np.uint8)
         for t in range(max_t // 2, max_t):
-            images = [big_chain[i][t] for i in range(n_samples)]
+            images = [chain[t][i] for i in range(n_samples)]
             decoded_images = [denormalize_img(img.permute(0, 2, 3, 1).detach().cpu().squeeze()).astype(np.uint8) for img in images]
             decoded_images = [cv2.cvtColor(img, cv2.COLOR_RGB2BGR) for img in decoded_images]
             decoded_images = [cv2.resize(img, (512, 512), interpolation=cv2.INTER_NEAREST) for img in decoded_images]
@@ -81,10 +109,10 @@ def test_DDPM_chain(model, beta, max_t, shape=(1, 1, 28, 28), n_samples=4, save_
             out.write(img)
         out.release()
 
-    return big_chain
+    return chain
 
 
-def test_stable_diffusion_chain(unet, vae, beta, max_t, texts_embeddings, latent_width=16, save_video=False):
+def test_stable_diffusion_chain(unet, vae, beta, max_t, texts_embeddings, latent_width=16, save_video=False, callback=None, fig=None):
     n_samples = len(texts_embeddings)
 
     latent_shape = (n_samples, 1, latent_width, latent_width)
@@ -120,18 +148,31 @@ def test_stable_diffusion_chain(unet, vae, beta, max_t, texts_embeddings, latent
             new_img = torch.clip(new_img, -1, 1)
             chain.append(new_img)
 
+            if t % 10 == 0 and callback is not None:
+                l, c = get_grid_size(n_samples)
+                fig.clf()
+                ax = fig.subplots(l, c)
+                images = [chain[-1][i] for i in range(n_samples)]
+                decoded_images = [vae.dec(img.view(1, 16 * 16)).cpu().detach().squeeze() for img in images]
+                decoded_images = [denormalize_img(img.permute(1, 2, 0)).astype(int) for img in decoded_images]
+                for j, img in enumerate(decoded_images):
+                    ax_index = (j // c, j % c)
+                    ax[ax_index].imshow(img)
+                    ax[ax_index].axis("off")
+                callback(100 - t / max_t * 100, fig)
+
         progress.update(time_task, visible=False)
 
-        fig, ax = plt.subplots(n_samples, 5)
-        for i in range(n_samples):
-            images = [chain[0][i], chain[-500][i], chain[-250][i], chain[-100][i], chain[-1][i]]
-            decoded_images = [vae.dec(img.view(1, 16 * 16)).cpu().detach().squeeze() for img in images]
-            decoded_images = [denormalize_img(img.permute(1, 2, 0)) for img in decoded_images]
-            for j, img in enumerate(decoded_images):
-                ax[i, j].imshow(img)
-                ax[i, j].axis("off")
-
-    plt.show()
+        if callback is None:
+            fig, ax = plt.subplots(n_samples, 5)
+            for i in range(n_samples):
+                images = [chain[0][i], chain[-500][i], chain[-250][i], chain[-100][i], chain[-1][i]]
+                decoded_images = [vae.dec(img.view(1, 16 * 16)).cpu().detach().squeeze() for img in images]
+                decoded_images = [denormalize_img(img.permute(1, 2, 0)) for img in decoded_images]
+                for j, img in enumerate(decoded_images):
+                    ax[i, j].imshow(img)
+                    ax[i, j].axis("off")
+            plt.show()
 
     if save_video:
         import cv2
@@ -149,3 +190,5 @@ def test_stable_diffusion_chain(unet, vae, beta, max_t, texts_embeddings, latent
 
             out.write(img)
         out.release()
+
+    return chain
